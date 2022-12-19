26918a4a116ae8bf2f8b510f0e24a9b96c14a540
Norman <normanramadhan3@gmail.com>
2022-12-20 06:02:22 +0700

    adding link

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,7 +2,11 @@ import { Container, TopLeft, BottomLeft, Hamburger } from "./../layout/styles";
 import { VelvetBanana } from "./../layout/VelvetBanana";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Overlay() {
   return (
@@ -14,16 +18,33 @@ export default function Overlay() {
           RAMADHAN —
         </h1>
         <p>Software Developer - Frontend</p>
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/works">Works</Link>
+          </li>
+        </ul> */}
       </TopLeft>
       <BottomLeft>
-        Get in Touch: 
+        Get in Touch:
         <a className="social-link" href="mailto:normanramadhan3@gmail.com">
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
-        <a className="social-link" href="https://www.instagram.com/normanrmdhn/">
-          <FontAwesomeIcon icon={faInstagramSquare} />
+        <a
+          className="social-link"
+          href="https://www.instagram.com/normanrmdhn/"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
         </a>
-        <a className="social-link" href="https://www.linkedin.com/in/nrmnramadhan/">
+        <a
+          className="social-link"
+          href="https://www.linkedin.com/in/nrmnramadhan/"
+        >
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
       </BottomLeft>
