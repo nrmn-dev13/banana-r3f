67a4bf8ff073e765d5e96fb73ca4998797cad2e1
Norman <normanramadhan3@gmail.com>
2022-12-19 19:56:41 +0700

    Adding fontawesome

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,15 @@
 import { Container, TopLeft, BottomLeft, Hamburger } from "./../layout/styles";
 import { VelvetBanana } from "./../layout/VelvetBanana";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function Overlay() {
   return (
     <Container>
       <TopLeft>
         <h1>
-          NORMAN 
+          NORMAN
           <br />
           RAMADHAN —
         </h1>
@@ -15,6 +18,8 @@ export default function Overlay() {
       <BottomLeft>
         A runtime deconstruction of{" "}
         <a href="https://playful.software">playful.software</a>
+        <FontAwesomeIcon icon={faEnvelope} />
+        <FontAwesomeIcon icon={faLinkedin} color="red" />
       </BottomLeft>
       <Hamburger>
         <div />
@@ -25,3 +30,4 @@ export default function Overlay() {
     </Container>
   );
 }
+
