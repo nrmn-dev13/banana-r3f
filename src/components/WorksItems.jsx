3d88e85b3a200f143d99d6919c7c4e5d42bb1ad8
Norman <normanramadhan3@gmail.com>
2023-01-03 18:57:53 +0700

    fix image height

--- src/components/WorksItems.jsx
+++ src/components/WorksItems.jsx
@@ -6,8 +6,8 @@ const WorksItems = (props) => {
           key={item.id}
           className="rounded-xl border-[#ffd863] bg-[#ffd863] overflow-hidden w-full"
         >
-          <figure className="max-h-[160px] min-h-[160px] overflow-hidden relative group">
-            <img src={item.img} alt="" />
+          <figure className="max-h-[160px] min-h-[160px] h-full overflow-hidden relative group">
+            <img src={item.img} className="h-full w-full object-cover" alt="" />
             {item.link === null ? null : ( // <p className="rounded-full p-2 flex items-center justify-center mx-auto max-w-[50%] w-full pointer-events-none">Unavailable</p>
               <a
                 className="rounded-full p-1 flex font-semibold items-center justify-center mx-auto absolute top-2.5 right-2.5 bg-[#ffd863] max-w-[25%] w-full transition-all opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-300"
