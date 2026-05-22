427ed28236c7701c9bcc9d3a8ca72500cd80258e
Norman <normanramadhan3@gmail.com>
2026-05-23 04:25:39 +0700

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/banana-r3f/'
-})
+  base: command === 'build' ? '/banana-r3f/' : '/'
+}))
